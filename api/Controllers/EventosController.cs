6c551108abe32bcaf3ee6fa36e7217b1cf0d9d84
SC <santiago@somosarado.com.ar>
2023-08-31 17:44:37 -0300

    initial

---
+++ api/Controllers/EventosController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EventosController : ControllerBase
+    {
+       
+        private readonly ILogger<EventosController> _logger;
+
+        public EventosController(ILogger<EventosController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "Get")]
+        public string Get()
+        {
+            return "OK";
+        }
+    }
+}
