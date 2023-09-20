6e9699e581da03acb4ac66532d0dcc6056fd9ae8
SC <santiago@somosarado.com.ar>
2023-09-20 15:47:21 -0300

    ++cors

--- api/Program.cs
+++ api/Program.cs
@@ -1,14 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+builder.Services.AddCors();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
+app.UseCors(
+        options => options.WithOrigins("http://localhost:3000/", "https://matevalley-eventos.netlify.app/").AllowAnyMethod()
+    );
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
